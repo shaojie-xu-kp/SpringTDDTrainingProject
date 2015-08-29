b7fe6a519ab01ea211f422001e45841d759a58f5
shaojie.xu <shaojie.xu@infinitagroup.it>
2015-08-29 16:57:55 +0200

    finish calculator test and development

--- src/test/java/www/shaojie/xu/StringCalculatorTest.java
+++ src/test/java/www/shaojie/xu/StringCalculatorTest.java
@@ -6,16 +6,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringTddTrainingProjectApplication.class)
 public class StringCalculatorTest {
 
-	@Test(expected = RuntimeException.class)
-    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-        StringCalculator.add("1,2,3");
-    }
+//	@Test(expected = RuntimeException.class)
+//    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+//        StringCalculator.add("1,2,3");
+//    }
 	
     @Test
     public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
@@ -26,6 +24,26 @@ public class StringCalculatorTest {
     @Test(expected = RuntimeException.class)
     public final void whenNonNumberIsUsedThenExceptionIsThrown() {
         StringCalculator.add("1,X");
+    }
+    
+    @Test
+    public final void whenEmptyStringIsUsedThenReturnValueIs0() {
+        Assert.assertEquals(0, StringCalculator.add(""));
+    }
+    
+    @Test
+    public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+        Assert.assertEquals(3, StringCalculator.add("3"));
+    }
+     
+    @Test
+    public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
+        Assert.assertEquals(3+6, StringCalculator.add("3,6"));
+    }
+    
+    @Test
+    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
     }
 
 }
